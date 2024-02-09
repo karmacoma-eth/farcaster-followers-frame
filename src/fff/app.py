283c087eb1c05e671dc5914f34f4e650c9feb7a5
karmacoma <karma@coma.lol>
2024-02-08 18:04:55 -0800

    properly handle grpc responses, handle pagination

--- src/fff/app.py
+++ src/fff/app.py
@@ -8,14 +8,11 @@ import seaborn as sns
 
 from dotenv import load_dotenv
 from fff.grpc import rpc_pb2_grpc as hub
-from fff.grpc.request_response_pb2 import LinksByTargetRequest
+from fff.grpc.request_response_pb2 import LinksByTargetRequest, MessagesResponse
+from fff.grpc.message_pb2 import MESSAGE_TYPE_LINK_ADD, MESSAGE_TYPE_LINK_REMOVE, FARCASTER_NETWORK_MAINNET
 from io import BytesIO
 
 load_dotenv()
-
-MESSAGE_TYPE_LINK_ADD = 'MESSAGE_TYPE_LINK_ADD'
-MESSAGE_TYPE_LINK_REMOVE = 'MESSAGE_TYPE_LINK_REMOVE'
-FARCASTER_NETWORK_MAINNET = 'FARCASTER_NETWORK_MAINNET'
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -28,18 +25,19 @@ def from_json_data(data: dict) -> pd.Series:
     messages = data['messages']
     logger.info(f'Loaded {len(messages)} messages')
 
-    for item in messages:
-        data = item['data']
-        if data['network'] != FARCASTER_NETWORK_MAINNET:
+    for message in messages:
+        data = message['data']
+        if data['network'] != 'FARCASTER_NETWORK_MAINNET':
+            logger.warn(f'Skipping message for network {data['network']} in {message}')
             continue
 
-        item_type = data['type']
-        if item_type == MESSAGE_TYPE_LINK_ADD:
+        message_type = data['type']
+        if message_type == 'MESSAGE_TYPE_LINK_ADD':
             follower_count += 1
-        elif item_type == MESSAGE_TYPE_LINK_REMOVE:
+        elif message_type == 'MESSAGE_TYPE_LINK_REMOVE':
             follower_count -= 1
         else:
-            logger.warn(f'Unknown message type {item_type} in {item}')
+            logger.warn(f'Unknown message type {message_type} in {message}')
 
         if follower_count < 0:
             logger.error(f'Negative follower count: {follower_count}')
@@ -57,24 +55,62 @@ def from_json_file(input_filename: str) -> pd.Series:
         return from_json_data(data)
 
 
+def from_grpc_data(response: MessagesResponse) -> pd.Series:
+    follower_counts = dict()
+    follower_count = 0
+
+    messages = response.messages
+    logger.info(f'Loaded {len(messages)} messages')
+
+    for message in messages:
+        data = message.data
+
+        if data.network != FARCASTER_NETWORK_MAINNET:
+            logger.warn(f'Skipping message for network {data.network} in {message}')
+            continue
+
+        message_type = data.type
+        if message_type == MESSAGE_TYPE_LINK_ADD:
+            follower_count += 1
+        elif message_type == MESSAGE_TYPE_LINK_REMOVE:
+            logger.info(f'lost a follower in {message}')
+            follower_count -= 1
+        else:
+            logger.warn(f'Unknown message type {message_type} in {message}')
+
+        if follower_count < 0:
+            logger.error(f'Negative follower count: {follower_count}')
+
+        timestamp = pd.to_datetime(data.timestamp, unit='s')
+        follower_counts[timestamp] = follower_count
+
+    # turn into a pandas series and sort by index
+    return pd.Series(follower_counts).sort_index()
+
+
 def from_grpc(fid: int) -> pd.Series:
     hub_rpc = os.getenv('HUB_RPC', None)
     if not hub_rpc:
         raise ValueError('HUB_RPC environment variable not set')
 
+    series = []
     with grpc.insecure_channel(hub_rpc) as channel:
         stub = hub.HubServiceStub(channel)
         page_token = None
 
         while True:
             request = LinksByTargetRequest(target_fid=fid, page_token=page_token)
-            response = stub.GetLinksByTarget(request)
-            logger.info(f'Loaded {len(response.messages)} messages over gRPC ({response.ByteSize()} bytes)')
+            response: MessagesResponse = stub.GetLinksByTarget(request)
+            series.append(from_grpc_data(response))
+            print(series[-1])
 
             if page_token := response.next_page_token:
                 continue
 
             break
+
+    return pd.concat(series)
+
 
 def generate_png(data: pd.Series, title: str) -> BytesIO:
     sns.set_style("whitegrid")
@@ -114,6 +150,8 @@ def main():
 
     else:
         dataset = from_grpc(int(user_input))
+
+    print(dataset)
 
     title = f'Follower count for {user_input}'
     output_filename = user_input.replace('.json', '.png')
